db1e3ec293d5a1e8dcbf6683415b4c8c65ce11ae
Victor22114 <victorkmikado709@gmail.com>
2024-11-27 16:59:28 -0300

    script.js

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ document.querySelectorAll('.faq-question').forEach(question => {
     });
 });
 
-
 document.getElementById('contact-form').addEventListener('submit', function (e) {
     e.preventDefault();
 
@@ -71,5 +70,16 @@ backToTopButton.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
+    });
+});
+
+const faqItems = document.querySelectorAll('.faq-item');
+
+
+faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+    question.addEventListener('click', () => {
+        
+        item.classList.toggle('active');
     });
 });
